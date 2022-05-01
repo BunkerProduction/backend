0ecb8f08b27eb5c3b5fe721b545d918a28f1f0cd
Timophey <55289941+TimoHub01@users.noreply.github.com>
2022-05-01 23:40:15 +0300

    ktor 2.0.1

--- Bunker/src/main/kotlin/com/example/Application.kt
+++ Bunker/src/main/kotlin/com/example/Application.kt
@@ -7,8 +7,23 @@ import com.example.plugins.configureSockets
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import org.koin.ktor.ext.Koin
-
+import io.ktor.events.EventDefinition
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.ApplicationStopped
+import io.ktor.server.application.createApplicationPlugin
+import io.ktor.server.application.install
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.Routing
+import org.koin.core.Koin
+import org.koin.core.KoinApplication
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.module.Module
+import org.koin.core.parameter.ParametersDefinition
+import org.koin.core.qualifier.Qualifier
+import org.koin.dsl.KoinAppDeclaration
 fun main() {
 //  embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
@@ -21,8 +36,7 @@ fun main() {
 @Suppress("unused")
 fun Application.module()
 {
-    install(Koin)
-    
+
     configureRouting()
     configureSecurity()
     configureSockets()
@@ -31,7 +45,6 @@ fun Application.module()
 
 
 }
-
 
 
 
